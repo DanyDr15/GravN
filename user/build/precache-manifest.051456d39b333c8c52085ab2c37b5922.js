74b3e4f8068333727b447b60c5057922afdf8bba
DanyDr15 <dapianchiche@gmail.com>
2019-08-15 03:41:50 -0500

    Subiendo el proyecto al servidor

---
+++ user/build/precache-manifest.051456d39b333c8c52085ab2c37b5922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "957fb590e3f9aa42178811c99bf3e352",
+    "url": "/static/media/marca.957fb590.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5df3df9f4d0d944495b",
+    "url": "/static/js/main.d5df3df9.chunk.js"
+  },
+  {
+    "revision": "54ba5c0de278b950c98a",
+    "url": "/static/js/1.54ba5c0d.chunk.js"
+  },
+  {
+    "revision": "54ba5c0de278b950c98a",
+    "url": "/static/css/1.997f9af9.chunk.css"
+  },
+  {
+    "revision": "b33bf6326139439bc60a43e6ec8bb382",
+    "url": "/index.html"
+  }
+];
